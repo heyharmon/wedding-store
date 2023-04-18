628b366765c9638e3f86832f00b7d8722a8832ff
Ryan Harmon <ryan@bloomcu.com>
2023-04-18 16:17:07 -0600

    Progress on CSS variable powered colors

--- pages/colors.vue
+++ pages/colors.vue
@@ -5,35 +5,35 @@
         <div class="mt-8">
             <h2 class="text-md font-semibold text-slate-900 mb-2">Base</h2>
             
-            <div class="flex overflow-hidden rounded-md" data-theme="base">   
-                <div class="grid h-16 w-16 place-items-end bg-primary-50 p-1 font-semibold leading-none text-primary-900">
+            <div class="flex gap-4" data-theme="base">   
+                <div class="grid h-16 w-16 place-items-end bg-primary-50 rounded-md p-1 font-semibold leading-none text-primary-900">
                 50
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-100 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-100 rounded-md p-1 font-semibold leading-none text-primary-900">
                 100
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-200 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-200 rounded-md p-1 font-semibold leading-none text-primary-900">
                 200
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-300 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-300 rounded-md p-1 font-semibold leading-none text-primary-900">
                 300
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-400 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-400 rounded-md p-1 font-semibold leading-none text-primary-900">
                 400
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-500 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-500 rounded-md p-1 font-semibold leading-none text-primary-50">
                 500
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-600 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-600 rounded-md p-1 font-semibold leading-none text-primary-50">
                 600
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-700 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-700 rounded-md p-1 font-semibold leading-none text-primary-50">
                 700
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-800 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-800 rounded-md p-1 font-semibold leading-none text-primary-50">
                 800
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-900 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-900 rounded-md p-1 font-semibold leading-none text-primary-50">
                 900
                 </div>
             </div>
@@ -42,35 +42,35 @@
         <div class="mt-8">
             <h2 class="text-md font-semibold text-slate-900 mb-2">Rainforest</h2>
 
-            <div class="flex overflow-hidden rounded-md" data-theme="rainforest">
-                <div class="grid h-16 w-16 place-items-end bg-primary-50 p-1 font-semibold leading-none text-primary-900">
+            <div class="flex gap-4" data-theme="rainforest">
+                <div class="grid h-16 w-16 place-items-end bg-primary-50 rounded-md p-1 font-semibold leading-none text-primary-900">
                 50
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-100 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-100 rounded-md p-1 font-semibold leading-none text-primary-900">
                 100
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-200 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-200 rounded-md p-1 font-semibold leading-none text-primary-900">
                 200
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-300 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-300 rounded-md p-1 font-semibold leading-none text-primary-900">
                 300
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-400 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-400 rounded-md p-1 font-semibold leading-none text-primary-900">
                 400
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-500 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-500 rounded-md p-1 font-semibold leading-none text-primary-50">
                 500
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-600 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-600 rounded-md p-1 font-semibold leading-none text-primary-50">
                 600
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-700 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-700 rounded-md p-1 font-semibold leading-none text-primary-50">
                 700
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-800 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-800 rounded-md p-1 font-semibold leading-none text-primary-50">
                 800
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-900 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-900 rounded-md p-1 font-semibold leading-none text-primary-50">
                 900
                 </div>
             </div>
@@ -79,35 +79,35 @@
         <div class="mt-8">
             <h2 class="text-md font-semibold text-slate-900 mb-2">Candy</h2>
 
-            <div class="flex overflow-hidden rounded-md" data-theme="candy">
-                <div class="grid h-16 w-16 place-items-end bg-primary-50 p-1 font-semibold leading-none text-primary-900">
+            <div class="flex gap-4" data-theme="candy">
+                <div class="grid h-16 w-16 place-items-end bg-primary-50 rounded-md p-1 font-semibold leading-none text-primary-900">
                 50
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-100 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-100 rounded-md p-1 font-semibold leading-none text-primary-900">
                 100
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-200 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-200 rounded-md p-1 font-semibold leading-none text-primary-900">
                 200
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-300 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-300 rounded-md p-1 font-semibold leading-none text-primary-900">
                 300
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-400 p-1 font-semibold leading-none text-primary-900">
+                <div class="grid h-16 w-16 place-items-end bg-primary-400 rounded-md p-1 font-semibold leading-none text-primary-900">
                 400
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-500 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-500 rounded-md p-1 font-semibold leading-none text-primary-50">
                 500
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-600 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-600 rounded-md p-1 font-semibold leading-none text-primary-50">
                 600
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-700 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-700 rounded-md p-1 font-semibold leading-none text-primary-50">
                 700
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-800 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-800 rounded-md p-1 font-semibold leading-none text-primary-50">
                 800
                 </div>
-                <div class="grid h-16 w-16 place-items-end bg-primary-900 p-1 font-semibold leading-none text-primary-50">
+                <div class="grid h-16 w-16 place-items-end bg-primary-900 rounded-md p-1 font-semibold leading-none text-primary-50">
                 900
                 </div>
             </div>
