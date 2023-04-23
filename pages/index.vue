86c0b81002d57018a85a5e80a4051ff5ce9cb6be
Ryan Harmon <ryan@bloomcu.com>
2023-04-22 18:22:33 -0600

    Cleanup the index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <h1>Homepage</h1>
-  <nuxt-link to="/example-page-mock">Example page</nuxt-link>
+  <section>
+    <h1 class="text-xl">Homepage</h1>
+
+    <div class="flex">
+      <Button to="/homepage">Homepage</Button>
+      <Button to="/page-1">Page 1</Button>
+    </div>
+
+    <VideoPlayer src="https://www.youtube.com/embed/MbZ7T71h76w"/>
+  </section>
 </template>
 
 <script setup></script>
