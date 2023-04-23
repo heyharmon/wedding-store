59682fcba1951c58e73776cace113c37eb595216
Ryan Harmon <ryan@bloomcu.com>
2023-04-22 18:22:26 -0600

    Add video player component

---
+++ components/Video/VideoPlayer.vue
@@ -0,0 +1,20 @@
+<template>
+  <iframe 
+    width="560" 
+    height="315" 
+    :src="src" 
+    title="Video player" 
+    frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    allowfullscreen
+  ></iframe>
+</template>
+
+<script setup>
+const props = defineProps({
+  src: {
+    type: String,
+    required: true
+  }
+})
+</script>
